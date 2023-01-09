94551e50d331f73a0d9491315faf60822402547b
Chromadesk <yisraeleliyah@gmail.com>
2023-01-09 15:10:51 -0500

    3 Panels made, infopanel still empty for now

--- src/App.js
+++ src/App.js
@@ -1,7 +1,69 @@
 import './App.css';
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col, Tabs, Tab } from 'react-bootstrap'
 import { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
+
+function PlayerPanel() {
+  return (
+    <Col id="playerpanel" className='panel' md="2">
+      <h2>Player Name</h2>
+      <h5 style={{ color: 'gray' }}>Survivor</h5>
+      <br></br>
+      <Container>
+        <Row><Col>Time</Col><Col style={{ textAlign: 'right' }}>0:00 PM</Col></Row>
+        <Row><Col>Weather</Col><Col style={{ textAlign: 'right' }}>Sunny</Col></Row>
+        <Row><Col>Miles Left</Col><Col style={{ textAlign: 'right' }}>100mi</Col></Row>
+        <Row><Col>Location</Col><Col style={{ textAlign: 'right' }}>Road</Col></Row>
+      </Container>
+      <br></br>
+      <h5>Stats</h5>
+      <Container>
+        <Row><Col>Health:</Col><Col style={{ textAlign: 'right' }}>100</Col></Row>
+        <Row><Col>Energy:</Col><Col style={{ textAlign: 'right' }}>100</Col></Row>
+        <Row><Col>Hunger:</Col><Col style={{ textAlign: 'right' }}>100</Col></Row>
+        <Row><Col>Thirst:</Col><Col style={{ textAlign: 'right' }}>100</Col></Row>
+      </Container>
+      <br></br>
+      <h5>Effects</h5>
+      <Container>
+        <Row style={{ color: 'gray' }}><Col>Status Effect</Col><Col style={{ textAlign: 'right' }}>Level</Col></Row>
+        <Row><Col>-</Col><Col style={{ textAlign: 'right' }}>0</Col></Row>
+      </Container>
+    </Col>
+  )
+}
+
+function GameArea() {
+  return (
+    <Col style={{ padding: '0px' }}>
+      <Container>
+        <Row id="maingame" className='mainpanel'>
+          maingame
+        </Row>
+        <Row id="viewpanel" className='viewpanel'>
+          <Container>
+            <Tabs defaultActiveKey="profile">
+              <Tab eventKey="profile" title="Profile">
+                <p>i just farteddd</p>
+              </Tab>
+              <Tab eventKey="inventory" title="Inventory">
+                <p>sketti yeuh yuhhhh</p>
+              </Tab>
+            </Tabs>
+          </Container>
+        </Row>
+      </Container>
+    </Col>
+  )
+}
+
+function InfoPanel() {
+  return (
+    <Col id="infopanel" className='panel' md="2">
+      infopanel
+    </Col>
+  )
+}
 
 class App extends Component {
   render() {
@@ -10,38 +72,9 @@ class App extends Component {
       <div>
         <Container fluid className='page'>
           <Row>
-            <Col id="playerpanel" className='panel' md="2">
-              <h2>Player Name</h2>
-              <h5 style={{ color: 'gray' }}>Survivor</h5>
-              <br></br>
-              <h5>Stats</h5>
-              <Container>
-                <Row><Col>Health:</Col><Col style={{ textAlign: 'right' }}>100</Col></Row>
-                <Row><Col>Energy:</Col><Col style={{ textAlign: 'right' }}>100</Col></Row>
-                <Row><Col>Hunger:</Col><Col style={{ textAlign: 'right' }}>100</Col></Row>
-                <Row><Col>Thirst:</Col><Col style={{ textAlign: 'right' }}>100</Col></Row>
-              </Container>
-              <br></br>
-              <h5>Effects</h5>
-              <Container>
-                <Row style={{ color: 'gray' }}><Col>Status Effect</Col><Col style={{ textAlign: 'right' }}>Level</Col></Row>
-                <Row><Col>-</Col><Col style={{ textAlign: 'right' }}>0</Col></Row>
-              </Container>
-
-            </Col>
-            <Col>
-              <Container>
-                <Row id="maingame" className='mainpanel'>
-                  maingame
-                </Row>
-                <Row id="viewpanel" className='viewpanel'>
-                  viewpanel
-                </Row>
-              </Container>
-            </Col>
-            <Col id="infopanel" className='panel' md="2">
-              infopanel
-            </Col>
+            <PlayerPanel />
+            <GameArea />
+            <InfoPanel />
           </Row>
         </Container>
       </div >
