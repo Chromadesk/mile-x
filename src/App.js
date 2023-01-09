97f0d736601a08bb3fa500aae0774644385d0dbc
Chromadesk <yisraeleliyah@gmail.com>
2023-01-09 14:05:07 -0500

    Starting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import { Container, Row, Col } from 'react-bootstrap'
+import { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+
+    return (
+      <div>
+        <Container fluid className='page'>
+          <Row>
+            <Col id="playerpanel" className='panel' md="2">
+              <h2>Player Name</h2>
+              <h5 style={{ color: 'gray' }}>Survivor</h5>
+              <br></br>
+              <h5>Stats</h5>
+              <Container>
+                <Row><Col>Health:</Col><Col style={{ textAlign: 'right' }}>100</Col></Row>
+                <Row><Col>Energy:</Col><Col style={{ textAlign: 'right' }}>100</Col></Row>
+                <Row><Col>Hunger:</Col><Col style={{ textAlign: 'right' }}>100</Col></Row>
+                <Row><Col>Thirst:</Col><Col style={{ textAlign: 'right' }}>100</Col></Row>
+              </Container>
+              <br></br>
+              <h5>Effects</h5>
+              <Container>
+                <Row style={{ color: 'gray' }}><Col>Status Effect</Col><Col style={{ textAlign: 'right' }}>Level</Col></Row>
+                <Row><Col>-</Col><Col style={{ textAlign: 'right' }}>0</Col></Row>
+              </Container>
+
+            </Col>
+            <Col>
+              <Container>
+                <Row id="maingame" className='mainpanel'>
+                  maingame
+                </Row>
+                <Row id="viewpanel" className='viewpanel'>
+                  viewpanel
+                </Row>
+              </Container>
+            </Col>
+            <Col id="infopanel" className='panel' md="2">
+              infopanel
+            </Col>
+          </Row>
+        </Container>
+      </div >
+    )
+  }
 }
 
 export default App;
